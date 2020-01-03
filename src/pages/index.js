6a3018eeba1f14adef249af210d934bb5f7cf2b7
Alex Yang <alexalexyang@gmail.com>
2020-01-03 00:31:13 +0000

    replaced index.js content with index.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,46 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+//     <Link to="/page-2/">Go to page 2</Link>
+//   </Layout>
+// )
 
-export default IndexPage
+// export default IndexPage
+
+export default function IndexPage({ data }) {
+  const post = data.markdownRemark
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{post.frontmatter.title}</h1>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+    </Layout>
+  )
+}
+
+export const postQuery = graphql`
+  query {
+    markdownRemark(frontmatter: { path: { eq: "/" } }) {
+      html
+      frontmatter {
+        path
+        title
+        author
+        date
+      }
+    }
+  }
+`
---
+++ src/pages/index.md
@@ -0,0 +1,8 @@
+---
+path: "/"
+date: "2020-02-03"
+title: "Supermeowkat"
+author: "Alex"
+---
+
+Welcome to art!
