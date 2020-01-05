14719f8a3a32e640ab0090d2899a345963bc5457
Alex Yang <alexalexyang@gmail.com>
2020-01-05 17:09:45 +0000

    removed console.log from so

--- src/components/seo.js
+++ src/components/seo.js
@@ -32,7 +32,6 @@ function SEO({ description, lang, meta, title, image }) {
 
   const { siteMetadata: data } = site
   const { node } = allFile.edges[0]
-  console.log(node.publicURL)
 
   const seo = {
     title: title || data.title,
