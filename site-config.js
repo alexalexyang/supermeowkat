4e1a91edb35e7a9837eff60fb72a209e4c82124b
Alex Yang <alexalexyang@gmail.com>
2020-01-10 14:27:39 +0000

    added if for env in site-config

--- site-config.js
+++ site-config.js
@@ -1,7 +1,16 @@
-module.exports = {
-  blog: false,
-  mailchimp: true,
-  noExhibition: false,
-  exhibition: false,
-  callForArtworks: true,
-}
+module.exports =
+  process.env.NODE_ENV === "development"
+    ? {
+        blog: true,
+        mailchimp: true,
+        noExhibition: false,
+        exhibition: false,
+        callForArtworks: true,
+      }
+    : {
+        blog: false,
+        mailchimp: true,
+        noExhibition: false,
+        exhibition: false,
+        callForArtworks: true,
+      }
